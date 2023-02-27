8fe016753d972c377d754e07c006bc51f6a0653f
yvnnbcker <yb-hfg@web.de>
2023-02-27 11:27:24 +0100

    meta data added

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,12 +5,17 @@ export default defineNuxtConfig({
     Generate: { fallback: true },
    // modules: ['@nuxt/content'],
     head: {
+        title: 'Yvonne Becker, Interaction Designerin',
         link: [
             {
             rel: 'stylesheet',
             href: 'https://use.typekit.net/jmp6nrg.css'
             },
         ],
+        meta: {
+            name: 'Yvonne Becker',
+            content: 'My Interaction Design Portfolio – UI, UX, Web Development, Exhibition and Print Projects',
+        },
     },
     css: [
         '~/assets/scss/main.scss'
@@ -21,3 +26,4 @@ export default defineNuxtConfig({
         }
     }
 });
+
