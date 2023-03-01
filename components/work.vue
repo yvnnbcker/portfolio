52aba4a7e4e11b41ed36145409f3af03058ce75b
yvnnbcker <yb-hfg@web.de>
2023-03-01 23:49:39 +0100

    adjustment footer & blobs & inpageav

--- components/work.vue
+++ components/work.vue
@@ -1,6 +1,6 @@
 <template>
-    <div id="projects">projects.</div>
-    <Navinpage />
+<Line id="projects"/>
+    <div class="projects-preview">projects.</div>
     <div id="card-grid">
         <Card title="more to come" date="03/2023" 
                 img="/more.png" alt="coomingsoon" 
@@ -8,7 +8,7 @@
                 href="/comingsoon"/>  
         <Card title="BA Silberwarenmuseum" date="02/2023" 
                 img="/ba.png" alt="ba" 
-                tag="iks" 
+                tag="exhibition" 
                 href="https://ausstellung.hfg-gmuend.de/w-2223/projekte/silberwarenmuseum-ott-pausersche-fabrik"/>
         <Card title="Anne Kowatsch" date="06/2021" 
                 img="/annekowatsch.png" alt="bsp" 
