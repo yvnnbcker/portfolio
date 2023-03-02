ea172af4ec394181836de1e808ba17e5b8c93bb7
yvnnbcker <yb-hfg@web.de>
2023-03-02 09:59:07 +0100

    preload images

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,47 +5,41 @@ export default defineNuxtConfig({
     Generate: { fallback: true },
    // modules: ['@nuxt/content'],
    meta: {
-    title: 'Yvonne Becker, Interaction Designerin',
-    name: 'Yvonne Becker',
-    content: 'My Interaction Design Portfolio – UI, UX, Web Development, Exhibition and Print Projects',
-    },
-    head: {
-        link: [
-            {
-            rel: 'stylesheet',
-            href: 'https://use.typekit.net/jmp6nrg.css'
-            },
-            {
-            rel: 'preload',
-            as: 'image',
-            href: '/yvonne-1.png'
-            },
-            {
-            rel: 'preload',
-            as: 'image',
-            href: '/yvonne-2.png'
-            },
-            {
-            rel: 'preload',
-            as: 'image',
-            href: '/yvonne-3.png'
-            }, 
-            {
-            rel: 'preload',
-            as: 'image',
-            href: '/yvonne-4.png'
-            },
-            {
-            rel: 'preload',
-            as: 'image',
-            href: '/yvonne-5.png'
-            },  
-        ],
-        meta: {
-            name: 'Yvonne Becker',
-            content: 'My Interaction Design Portfolio – UI, UX, Web Development, Exhibition and Print Projects',
-            },
-    },
+        title: 'Yvonne Becker, Interaction Designerin',
+        name: 'description',
+        content: 'My Interaction Design Portfolio – UI, UX, Web Development, Exhibition and Print Projects',
+        },
+    link: [
+        {
+        rel: 'stylesheet',
+        href: 'https://use.typekit.net/jmp6nrg.css'
+        },
+        {
+        rel: 'preload',
+        as: 'image',
+        href: '/yvonne-1.png'
+        },
+        {
+        rel: 'preload',
+        as: 'image',
+        href: '/yvonne-2.png'
+        },
+        {
+        rel: 'preload',
+        as: 'image',
+        href: '/yvonne-3.png'
+        }, 
+        {
+        rel: 'preload',
+        as: 'image',
+        href: '/yvonne-4.png'
+        },
+        {
+        rel: 'preload',
+        as: 'image',
+        href: '/yvonne-5.png'
+        },  
+    ],    
     css: [
         '~/assets/scss/main.scss'
         ],
