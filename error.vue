c3507475349f1aef8b820e4f3f8f18141933312e
yvnnbcker <yb-hfg@web.de>
2023-03-07 14:45:14 +0100

    error page base

---
+++ error.vue
@@ -0,0 +1,19 @@
+<template>
+    <div id="error-container">
+      <div class="error-message">
+        Ooops, seems like an error occurred.
+      </div>
+      <div class="error-code">      
+        <b>{{ error.statusCode }}</b> – {{ error.message }}
+      </div>
+      <Buttonicon href="/" icon="arrow-left">
+            go back to main page
+      </Buttonicon>
+    </div>
+  </template>
+  
+  <script setup>
+    defineProps(['error'])
+  </script>
+
+  
