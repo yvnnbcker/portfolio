ae072523e4150287127dc910a85239f5614407c6
yvnnbcker <yb-hfg@web.de>
2023-03-28 17:17:33 +0200

    lebenslauf events zusammen

--- pages/about.vue
+++ pages/about.vue
@@ -23,11 +23,8 @@
         </div>
         <div class="career-content">
             <div class="career-text">
-                <div class="event"><h1>*</h1></div>
-            </div>
-            <div class="career-text">
+                <div class="event"><h1>*</h1></div>                
                 <div class="event"><h1>[…]</h1></div>
-
             </div>
             <div class="career-currently">            
                 <div class="career-text">
