50e500dafa7aedcad68b208a46249f69799295fc
Yvonne Becker <73485093+yvnnbcker@users.noreply.github.com>
2023-04-26 16:10:09 +0200

    blum icons done

--- components/image-text.vue
+++ components/image-text.vue
@@ -1,16 +1,16 @@
 <script setup>
-    const bullet = defineProps(['src', 'alt', 'title', 'bullet1', 'bullet2', 'bullet3', 'bullet4', 'bullet5', 'bullet6']);
+    const project = defineProps(['img', 'alt', 'headline', 'desc']);
 </script>
 
 <template>
-    <div class="image-text">
-        <img :src="bullet.src" :alt="bullet.alt">
-        <h3 v-if="bullet.title!==undefined" class="bullet-title">{{ bullet.title }}</h3>
-        <div v-if="bullet.bullet1!==undefined" class="single-bullet"> {{ bullet.bullet1 }}</div>
-        <div v-if="bullet.bullet2!==undefined" class="single-bullet"> {{ bullet.bullet2 }}</div>
-        <div v-if="bullet.bullet3!==undefined" class="single-bullet"> {{ bullet.bullet3 }}</div>
-        <div v-if="bullet.bullet4!==undefined" class="single-bullet"> {{ bullet.bullet4 }}</div>
-        <div v-if="bullet.bullet5!==undefined" class="single-bullet"> {{ bullet.bullet5 }}</div>
-        <div v-if="bullet.bullet6!==undefined" class="single-bullet"> {{ bullet.bullet6 }}</div>
+    <div>
+        <div class="image-text">
+            <h3 v-if="project.headline!==undefined" class="text-headline">{{ project.headline }}</h3>
+            <slot />
+        </div>
+        <div class="project-image">
+            <img :src=project.img :alt=project.alt />
+            <div class="image-desc">{{ project.desc }}</div>
+        </div>
     </div>
 </template>
--- pages/projects/blum-icons.vue
+++ pages/projects/blum-icons.vue
@@ -8,27 +8,26 @@
         img="/blum_icons-hero.png"
         alt="two setting icons in orange and dark blue"
       />
-      <div id="project-text-container">
-        <HeadlineText
-          headline="? Problem"
-          text="The previous icons look very different – especially in terms of size, stroke width and style. Therefore, they do not result in a harmonious visual language."/>
-        <HeadlineText
-          headline="! Solution"
-          text="Establishing a grid system for the icons, according to which all new icons are getting created. Some icons get matched into the new grid system, others are getting completely redesigned. A few icons needed to be invented for special cases."/>
+      <div class="project-grid-container">
+        <ImageText
+          headline="?Problem"
+          img="/blum_icons-old.png"
+          alt="image of old blum icons"
+          desc="icons before redesign">
+          The previous icons look very different – especially in terms of size, stroke width and style. Therefore, they do not result in a harmonious visual language.
+        </ImageText>
+        <ImageText
+          headline="!Solution"
+          img="/blum_icons-new.png"
+          alt="image ofnew blum icons"
+          desc="icons after redesign">
+          Establishing a grid system for icons, according to which all new icons are getting created. Some icons get matched into the new grid system, others are getting completely redesigned. A few icons needed to be invented for special cases.
+        </ImageText>
       </div>
       <ImageUno
         img="/blum_icons-anatomy.png"
         alt="image of grids and icon forms"
-        desc="establishing an icon grid"
-        class="img-padding"
-        />
-      <ImagesTwo
-        img1="/blum_icons-old.png"
-        alt1="image of old blum icons"
-        desc1="icons before redesign"
-        img2="/blum_icons-new.png"
-        alt2="image of new blum icons"
-        desc2="icons after redesign"
+        desc="established an icon grid"
         />
     </div>
     <Infobox
@@ -40,7 +39,6 @@
         tool3="/logo-jira.svg"
         href="https://www.blum.com/"
         buttontext="visit website"
-        buttoncolor="button-orange"
         />
     </div>
 </template>
