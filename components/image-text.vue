3d6fe508f0d5c484c36caf3e5abb45a94c6cdf03
Yvonne Becker <73485093+yvnnbcker@users.noreply.github.com>
2023-04-26 15:44:28 +0200

    changed emty to undefined

--- components/image-text.vue
+++ components/image-text.vue
@@ -5,12 +5,12 @@
 <template>
     <div class="image-text">
         <img :src="bullet.src" :alt="bullet.alt">
-        <h3 v-if="bullet.title!=empty" class="bullet-title">{{ bullet.title }}</h3>
-        <div v-if="bullet.bullet1!=empty" class="single-bullet"> {{ bullet.bullet1 }}</div>
-        <div v-if="bullet.bullet2!=empty" class="single-bullet"> {{ bullet.bullet2 }}</div>
-        <div v-if="bullet.bullet3!=empty" class="single-bullet"> {{ bullet.bullet3 }}</div>
-        <div v-if="bullet.bullet4!=empty" class="single-bullet"> {{ bullet.bullet4 }}</div>
-        <div v-if="bullet.bullet5!=empty" class="single-bullet"> {{ bullet.bullet5 }}</div>
-        <div v-if="bullet.bullet6!=empty" class="single-bullet"> {{ bullet.bullet6 }}</div>
+        <h3 v-if="bullet.title!==undefined" class="bullet-title">{{ bullet.title }}</h3>
+        <div v-if="bullet.bullet1!==undefined" class="single-bullet"> {{ bullet.bullet1 }}</div>
+        <div v-if="bullet.bullet2!==undefined" class="single-bullet"> {{ bullet.bullet2 }}</div>
+        <div v-if="bullet.bullet3!==undefined" class="single-bullet"> {{ bullet.bullet3 }}</div>
+        <div v-if="bullet.bullet4!==undefined" class="single-bullet"> {{ bullet.bullet4 }}</div>
+        <div v-if="bullet.bullet5!==undefined" class="single-bullet"> {{ bullet.bullet5 }}</div>
+        <div v-if="bullet.bullet6!==undefined" class="single-bullet"> {{ bullet.bullet6 }}</div>
     </div>
 </template>
