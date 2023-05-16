059c55be3e37ba743114933defffed0ebb9d0a16
Yvonne Becker <73485093+yvnnbcker@users.noreply.github.com>
2023-05-16 18:10:47 +0200

    project update

--- components/projects.vue
+++ components/projects.vue
@@ -2,6 +2,11 @@
         <div id="projects">
                 <Sectiontitle>projects.</Sectiontitle>
                 <div id="card-grid"> 
+                        <Card title="Podcast BA Thesis" date="05/2023" 
+                                img="/logo-smartinnovation.jpg" alt="logo from smart innovation podcast" 
+                                tag="podcast" 
+                                href="https://www.klausreichert.de/verbinden-von-analogen-und-digitalen-welten-im-museum/"                                
+                                target="_blank"/> 
                         <Card title="BA Silverware Museum" date="02/2023" 
                                 img="/ba.png" alt="hand with smartphone using ar" 
                                 tag="exhibition" 
@@ -23,11 +28,7 @@
                         <Card title="Green Party Schorndorf" date="03/2021" 
                                 img="/bsp.png" alt="mockup of green party schorndorf website" 
                                 tag="coming soon | web" 
-                                href="/comingsoon"/>
-                        <Card title="moodle Redesign" date="06/2020" 
-                                img="/moodle-preview.png" alt="mockup of moodle application" 
-                                tag="coming soon | app" 
-                                href="/comingsoon"/>     
+                                href="/comingsoon"/>    
                 </div>
         </div>
 </template>
